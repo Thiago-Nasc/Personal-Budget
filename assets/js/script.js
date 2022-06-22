9675666ebc0c3ea17c0de757f2d9f2929d26c816
thiago <tgn450484@gmail.com>
2022-06-22 15:14:24 -0300

    recuperando valores de entrada

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Cadastrando despesas
+function cadastrarDespesa() {
+
+    // Recuperando valores de entrada
+    let ano = document.querySelector('#ano');
+    let mes = document.querySelector('#mes');
+    let dia = document.querySelector('#dia');
+    let tipo = document.querySelector('#tipo');
+    let descricao = document.querySelector('#descricao');
+    let valor = document.querySelector('#valor');
+    
+}
